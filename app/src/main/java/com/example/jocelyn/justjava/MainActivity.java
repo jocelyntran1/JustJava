c1fa2a0fcc2d5ad73b4b13c1fa1c3742274f5c52
Jocelyn Tran <jocelyn.tran1@gmail.com>
2016-07-29 23:37:10 -0700

    Updated price calculations

--- app/src/main/java/com/example/jocelyn/justjava/MainActivity.java
+++ app/src/main/java/com/example/jocelyn/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jocelyn.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,9 +28,31 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         EditText nameEditText = (EditText) findViewById( R.id.name_edit_text );
         String name = nameEditText.getText().toString();
-        String priceMessage = "Name: " + name + "\nQuantity: " + quantity + "\nTotal: $" +
-                (quantity*5) + "\nThank you!";
+        int price = calculateTotalPrice();
+        String priceMessage = "Name: " + name + "\nQuantity: " + quantity + "\nTotal: $" + price +
+                "\nThank you!";
         displayMessage(priceMessage);
+    }
+
+    /**
+     * This method calculates the total price.
+     */
+    private int calculateTotalPrice() {
+        int price = 5;
+
+        // Chocalate costs $2 per cup
+        CheckBox chocolateCB = (CheckBox) findViewById( R.id.chocolate_checkBox );
+        if( chocolateCB.isChecked() ) {
+            price += 2;
+        }
+
+        // Whipped cream costs $1 per cup
+        CheckBox creamCB = (CheckBox) findViewById( R.id.whipped_cream_checkBox );
+        if( creamCB.isChecked() ) {
+            price++;
+        }
+
+        return price * quantity;
     }
 
     /**
